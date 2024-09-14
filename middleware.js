4def25900bc17feb1482ae7324a900e79b95decf
KYs <amitkys59@gmail.com>
2024-09-15 03:00:41 +0530

    comment useful stuff for further dubbing

--- middleware.js
+++ middleware.js
@@ -1,7 +1,7 @@
 module.exports.isLoggedIn = (req, res, next) => {
     // console.log(req);
     if (!req.isAuthenticated()) {
-        console.log('req original url', req.originalUrl);
+        // console.log('req original url', req.originalUrl);
         req.session.redirectUrl = req.originalUrl;
         req.flash("error", "You need to login first");
         return res.redirect('/login');
@@ -10,8 +10,8 @@ module.exports.isLoggedIn = (req, res, next) => {
 }
 
 module.exports.saveRedirectUrl = (req, res, next) => {
-    console.log('hello')
-    console.log(req.session.redirectUrl);
+    // console.log('hello')
+    // console.log(req.session.redirectUrl);
     if(req.session.redirectUrl){
         res.locals.redirectUrl = req.session.redirectUrl;
         
