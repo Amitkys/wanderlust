a76debd4921ccd79aeebaa7956e9a7160c901016
KYs <amitkys59@gmail.com>
2024-09-15 02:41:37 +0530

    added path before login

--- middleware.js
+++ middleware.js
@@ -1,5 +1,7 @@
 module.exports.isLoggedIn = (req, res, next) => {
+    // console.log(req);
     if (!req.isAuthenticated()) {
+        req.session.redirectUrl = req.originalUrl;
         req.flash("error", "You need to login first");
         return res.redirect('/login');
     }
