aa254d7931541e5b032280c38e5bffa345a5351b
KYs <amitkys59@gmail.com>
2024-09-15 03:00:01 +0530

    redirecting user to actionable url

--- middleware.js
+++ middleware.js
@@ -1,9 +1,21 @@
 module.exports.isLoggedIn = (req, res, next) => {
     // console.log(req);
     if (!req.isAuthenticated()) {
+        console.log('req original url', req.originalUrl);
         req.session.redirectUrl = req.originalUrl;
         req.flash("error", "You need to login first");
         return res.redirect('/login');
+    }
+    next();
+}
+
+module.exports.saveRedirectUrl = (req, res, next) => {
+    console.log('hello')
+    console.log(req.session.redirectUrl);
+    if(req.session.redirectUrl){
+        res.locals.redirectUrl = req.session.redirectUrl;
+        
+
     }
     next();
 }
--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const User = require('../models/user.js');
 const wrapAsync = require('../utils/wrapAsync.js');
 const passport = require('passport');
+const {saveRedirectUrl} = require('../middleware.js');
 
 router.get("/signup", (req, res) => {
     res.render("user/signup.ejs");
@@ -32,14 +33,14 @@ router.post("/signup", wrapAsync( async(req, res) => {
 router.get("/login", (req, res) => {
     res.render("user/login.ejs");
 });
-router.post("/login",
+router.post("/login", saveRedirectUrl,
     passport.authenticate("local", {
         failureRedirect: "/login",
         failureFlash: true,
     }),
     async (req, res) => {
         req.flash("success", "login successful");
-        res.redirect("/listings");
+        res.redirect(res.locals.redirectUrl);
     })
 router.get('/logout', (req, res) => {
     req.logout((err) => {
